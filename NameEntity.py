4f0a8c4a79167846c399d283c2ed43750517d360
yushijie <yushijie@buaa.edu.cn>
2017-10-27 20:39:19 +0800

    refactor and add some annotation

--- NameEntity.py
+++ NameEntity.py
@@ -2,15 +2,23 @@ import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 from gensim.models import word2vec
-from sklearn import preprocessing
+from sklearn import preprocessing,decomposition
 from mpl_toolkits.mplot3d import Axes3D
 
 def get_similarity_mat():
+    '''
+
+    :return:the matrix of similarity
+    '''
+    #load the model that saved before
     model = word2vec.Word2Vec.load("./model.txt")
+    #read name_list
     names = open('names.txt')
     name_list = []
     for name in names:
         name_list.append(name[:-1])
+
+    #compute similarity matrix
     name_similarity_array = []
     for name_row in name_list:
         tmp = []
@@ -18,10 +26,17 @@ def get_similarity_mat():
              sim = model.similarity(name_row,name_column)
              tmp.append(sim)
         name_similarity_array.append(tmp)
+    #normalize
     name_mat = preprocessing.MinMaxScaler().fit_transform(np.mat(name_similarity_array))
     return name_mat,name_list
 
 def save_similar_mat(name_mat,name_list):
+    '''
+    save matrix
+    :param name_mat:
+    :param name_list:
+    :return:
+    '''
     f = open('./human_similar_vector.csv','w')
     for i in range(len(name_list)+1):
         for j in range(len(name_list)+1):
@@ -38,7 +53,14 @@ def save_similar_mat(name_mat,name_list):
             else:f.write(',')
 
 def visulize(name_mat,name_list):
+    #pcva
+    # pca = decomposition.PCA(n_components=3)
+    # pca.fit(name_mat)
+    # U = pca.transform(name_mat)
+    #svd
     U,S,V = np.linalg.svd(name_mat)
+
+    #draw 3D picture
     ax = Axes3D(plt.figure())
     for i in range(len(name_list)):
         ax.scatter(U[i, 0], U[i, 1],U[i,2])
@@ -47,7 +69,16 @@ def visulize(name_mat,name_list):
 
 
 
-def visulize2(name_mat,name_list,name,layer,accurate):
+def visulize2(name_mat,name_list,name,layer = 2,accurate = 0.9):
+    '''
+
+    :param name_mat:
+    :param name_list:
+    :param name: the center of the graph
+    :param layer: decide the layer number of the graph
+    :param accurate: the similarity between the center with next layer
+    :return:
+    '''
     graph = nx.Graph()
     add_element(graph,name_mat,name_list,name,layer,accurate)
     nx.draw(graph, node_size=100, node_color='g', with_labels=True,
@@ -55,6 +86,17 @@ def visulize2(name_mat,name_list,name,layer,accurate):
     plt.show()
 
 def add_element(graph,name_mat,name_list,name,layer,accurate):
+    '''
+    add element to the graph,including node and edge
+
+    :param graph:
+    :param name_mat:
+    :param name_list:
+    :param name:
+    :param layer:
+    :param accurate:
+    :return:
+    '''
     graph.add_node(name)
     if layer == 1:return
     pos = name_list.index(name)
@@ -62,9 +104,9 @@ def add_element(graph,name_mat,name_list,name,layer,accurate):
         if name_mat[pos, i] >= accurate:
             graph.add_node(name_list[i])
             graph.add_edge(name, name_list[i])
-            add_element(graph,name_mat,name_list,name_list[i],layer-1,accurate+0.03)
+            add_element(graph,name_mat,name_list,name_list[i],layer-1,accurate)
 
 name_mat,name_list = get_similarity_mat()
-#save_similar_mat(name_mat,name_list)
+save_similar_mat(name_mat,name_list)
 #visulize(name_mat,name_list)
-visulize2(name_mat,name_list,'Daenerys',3,0.95)
+#visulize2(name_mat,name_list,'Snow')
